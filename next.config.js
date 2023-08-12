e46c91492b7e32bcb57e803010db35f77c6de05c
kam6ri <uesmgn@gmail.com>
2023-08-12 21:49:12 +0900

    依存パッケージを更新

--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // ... other next config items
 
-module.exports = nextConfig
+    webpack: config => {
+        // ... other rules
+
+        config.module.rules.push({
+        test: /\.svg$/,
+        use: [
+            {
+                loader: '@svgr/webpack'
+            },
+            {
+                loader: 'file-loader',
+                options: {
+                    name: 'static/[path][name].[ext]'
+                }
+            }
+        ],
+        type: 'javascript/auto',
+        issuer: {
+            and: [/\.(ts|tsx|js|jsx|md|mdx)$/]
+        }
+        });
+        return config;
+    }
+};
+
+module.exports = nextConfig;
