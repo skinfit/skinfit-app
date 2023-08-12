6687b0ccf1fb0e15093c928ec55087c9987f6a65
kam6ri <uesmgn@gmail.com>
2023-08-12 21:49:45 +0900

    SVGRの設定を追加

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,13 @@
 import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: [
+    // "../src/**/*.mdx", 
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
@@ -14,6 +16,29 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  // staticDirs: [{ from: '../public', to: '/public' }],
+  // https://github.com/storybookjs/storybook/issues/18557
+  core: {},
+  webpackFinal: async config => {
+    const imageRule = config.module?.rules?.find(rule => {
+      const test = (rule as { test: RegExp }).test
+
+      if (!test) {
+        return false
+      }
+
+      return test.test('.svg')
+    }) as { [key: string]: any }
+
+    imageRule.exclude = /\.svg$/
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+
+    return config
   },
 };
 export default config;
