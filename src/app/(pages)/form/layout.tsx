65f907679b4e3df43572328efd94812b2855e2b9
kam6ri <uesmgn@gmail.com>
2023-10-24 20:46:09 +0900

    v1.0.1 release

--- src/app/(pages)/form/layout.tsx
+++ src/app/(pages)/form/layout.tsx
@@ -1,4 +1,10 @@
-import { FormProvider } from "@/features/form/components/FormProvider"
+import { FormProvider } from "@/features/form/components/FormProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'SkinFit - スキンケア製品のレコメンドエンジン',
+    description: 'SkinFit',
+}
 
 export default function FormLayout({
     children,
